ac6d4fd00cbc8e8f244515caff83371f6b9baf50
Pawel Lampe <pawel.lampe@gmail.com>
2023-01-04 12:54:23 +0100

    Add credits menu

---
+++ source/main-menu/Credits.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://source/main-menu/Main.tscn")
--- source/main-menu/Main.gd
+++ source/main-menu/Main.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://source/main-menu/Credits.tscn")
