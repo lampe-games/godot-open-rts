adf9a20edc1254d67dcfe3f1d7a41bb290b3718b
Pawel Lampe <pawel.lampe@gmail.com>
2023-01-12 18:44:08 +0100

    Add missing unit colors

--- source/match/units/Drone.gd
+++ source/match/units/Drone.gd
@@ -2,4 +2,8 @@ extends Area3D
 
 const SIGHT_RANGE = 10.0
 
-var color = null
+var color = null:
+	set(a_color):
+		var material = StandardMaterial3D.new()
+		material.albedo_color = a_color
+		find_child("MeshInstance3D").material_override = material
