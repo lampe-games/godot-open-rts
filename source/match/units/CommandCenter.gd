d7ecf0f1b1edf0572efe8dfb8df94e2793a11836
Pawel Lampe <pawel.lampe@gmail.com>
2023-01-23 09:00:29 +0100

    Apply spacekit models to units vol. 2

--- source/match/units/CommandCenter.gd
+++ source/match/units/CommandCenter.gd
@@ -4,11 +4,7 @@ const ManagingProduction = preload("res://source/match/units/actions/ManagingPro
 
 const SIGHT_RANGE = 10.0
 
-var color:
-	set(a_color):
-		var material = StandardMaterial3D.new()
-		material.albedo_color = a_color
-		find_child("MeshInstance3D2").material_override = material
+var color = null
 
 
 func _ready():
--- source/match/units/Drone.gd
+++ source/match/units/Drone.gd
@@ -2,8 +2,4 @@ extends "res://source/match/units/Unit.gd"
 
 const SIGHT_RANGE = 10.0
 
-var color = null:
-	set(a_color):
-		var material = StandardMaterial3D.new()
-		material.albedo_color = a_color
-		find_child("MeshInstance3D").material_override = material
+var color = null
