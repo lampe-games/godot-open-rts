094cfce3c219ec37f5c9da9e2756dd541a54b8ab
Pawel Lampe <pawel.lampe@gmail.com>
2023-01-26 18:28:12 +0100

    Refactor MouseClickAnimationsHandler

--- source/match/handlers/MouseClickAnimationsHandler.gd
+++ source/match/handlers/MouseClickAnimationsHandler.gd
@@ -7,7 +7,7 @@ func _ready():
 	MatchSignals.terrain_targeted.connect(_on_terrain_targeted)
 
 
-func _on_terrain_targeted(position):
+func _on_terrain_targeted(target_position):
 	if (
 		get_tree()
 		. get_nodes_in_group("selected_units")
@@ -16,5 +16,5 @@ func _on_terrain_targeted(position):
 	):
 		return
 	var node = MouseClickAnimation.instantiate()
-	node.global_transform = Transform3D(Basis(), position)
+	node.global_transform = Transform3D(Basis(), target_position)
 	add_child(node)
