091df2e2639d21c17669a9a8fd90101c848c8e12
Pawel Lampe <pawel.lampe@gmail.com>
2023-01-28 23:45:44 +0100

    Make hp bar auto-show when hp changes

--- source/match/units/traits/HealthBar.gd
+++ source/match/units/traits/HealthBar.gd
@@ -1,6 +1,7 @@
-# TODO: dynamic show/hide on hp changes
 @tool
 extends Node3D
+
+const BAR_AUTO_VISIBILITY_DURATION = 2.0
 
 @export var size = Vector2(200, 20):
 	set(value):
@@ -8,8 +9,11 @@ extends Node3D
 		find_child("SubViewport").size = size
 		find_child("ActualBar").size = size
 
+var _bar_value_initialized = false
+
 @onready var _unit = get_parent()
 @onready var _actual_bar = find_child("ActualBar")
+@onready var _visibility_timer = find_child("Timer")
 
 
 func _ready():
@@ -20,15 +24,28 @@ func _ready():
 	_unit.selected.connect(_on_unit_selected)
 	_unit.deselected.connect(_on_unit_deselected)
 	_unit.hp_changed.connect(_on_hp_changed)
+	_visibility_timer.timeout.connect(_on_visibility_timer_timeout)
 
 
 func _recalulate_bar_value():
-	_actual_bar.value = (
-		float(_unit.hp) / _unit.hp_max if _unit.hp != null and _unit.hp_max != null else 0
-	)
+	if _unit.hp == null or _unit.hp_max == null:
+		return
+	var old_value = _actual_bar.value
+	_actual_bar.value = float(_unit.hp) / _unit.hp_max
+	if _bar_value_initialized and old_value != _actual_bar.value:
+		_show_for_a_while()
+	_bar_value_initialized = true
+
+
+func _show_for_a_while():
+	if visible:
+		return
+	show()
+	_visibility_timer.start(BAR_AUTO_VISIBILITY_DURATION)
 
 
 func _on_unit_selected():
+	_visibility_timer.stop()
 	show()
 
 
@@ -38,3 +55,7 @@ func _on_unit_deselected():
 
 func _on_hp_changed():
 	_recalulate_bar_value()
+
+
+func _on_visibility_timer_timeout():
+	hide()
