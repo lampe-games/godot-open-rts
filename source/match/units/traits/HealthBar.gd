3a7fc0243dad7986e9649f8b85356774eebedfd1
Pawel Lampe <pawel.lampe@gmail.com>
2023-01-31 19:28:10 +0100

    Rework HP bars to use gradient textures instead od viewport textures

--- source/match/units/traits/HealthBar.gd
+++ source/match/units/traits/HealthBar.gd
@@ -6,8 +6,8 @@ const BAR_AUTO_VISIBILITY_DURATION = 2.0
 @export var size = Vector2(200, 20):
 	set(value):
 		size = value
-		find_child("SubViewport").size = size
-		find_child("ActualBar").size = size
+		find_child("ActualBar").texture.width = size.x
+		find_child("ActualBar").texture.height = size.y
 
 var _bar_value_initialized = false
 
@@ -30,9 +30,11 @@ func _ready():
 func _recalulate_bar_value():
 	if _unit.hp == null or _unit.hp_max == null:
 		return
-	var old_value = _actual_bar.value
-	_actual_bar.value = float(_unit.hp) / _unit.hp_max
-	if _bar_value_initialized and old_value != _actual_bar.value:
+	var old_value = _actual_bar.texture.gradient.get_offset(1)
+	var new_value = float(_unit.hp) / _unit.hp_max
+	new_value = new_value if not is_equal_approx(new_value, 1.0) else 1.1  # fixing 1px gap
+	_actual_bar.texture.gradient.set_offset(1, new_value)
+	if _bar_value_initialized and old_value != new_value:
 		_show_for_a_while()
 	_bar_value_initialized = true
 
