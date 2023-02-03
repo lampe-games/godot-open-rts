a385afdb8165ddd13661fd254bb49f496475571a
Pawel Lampe <pawel.lampe@gmail.com>
2023-02-03 20:46:24 +0100

    Improve 'Rocket' projectile

--- source/match/units/projectiles/Rocket.gd
+++ source/match/units/projectiles/Rocket.gd
@@ -1,28 +1,30 @@
-# TODO: use animation player for better animation
-# TODO: add particles for trail
+# TODO: investigate if anything can be done w/ particles in (0,0,0)
 extends Node3D
-
-const ANIMATION_DURATION_S = 0.5
 
 var target_unit = null
 
 @onready var _unit = get_parent()
 @onready var _path = find_child("Path3D")
-@onready var _path_follow = find_child("PathFollow3D")
+@onready var _animation_player = find_child("AnimationPlayer")
+@onready var _rocket = find_child("MeshInstance3D")
+@onready var _particles = find_child("GPUParticles3D")
 
 
 func _ready():
 	assert(target_unit != null)
+	_rocket.hide()
+	_particles.hide()
 	target_unit.died.connect(queue_free)
+	_animation_player.animation_finished.connect(func(_animation): queue_free())
 	_setup_path()
+	# wait 2 frames for path curve setup so that path follow has correct transform
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+	_animation_player.play("animate")
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	_path.curve.set_point_position(1, target_unit.global_position)
-	var progress_ratio_delta = delta / ANIMATION_DURATION_S
-	_path_follow.progress_ratio = min(1.0, _path_follow.progress_ratio + progress_ratio_delta)
-	if _path_follow.progress_ratio + progress_ratio_delta >= 1.0:
-		_handle_hit()
 
 
 func _setup_path():
@@ -31,9 +33,9 @@ func _setup_path():
 		if _unit.find_child("ProjectileOrigin") == null
 		else _unit.find_child("ProjectileOrigin").global_position
 	)
-	_path.curve.set_point_position(0, projectile_origin)
-	_path.curve.set_point_position(1, target_unit.global_position)
+	_path.curve.add_point(projectile_origin)
+	_path.curve.add_point(target_unit.global_position)
 
 
-func _handle_hit():
+func _perform_hit():
 	target_unit.hp -= _unit.attack_damage
