1e57161cfbedb28c5dd7881d7b5928228ac1fe56
Pawel Lampe <pawel.lampe@gmail.com>
2023-03-23 21:24:20 +0100

    Balance unit properties, closes #17

--- source/match/MatchConstants.gd
+++ source/match/MatchConstants.gd
@@ -32,30 +32,30 @@ class Units:
 	const PRODUCTION_COSTS = {
 		"res://source/match/units/Worker.tscn":
 		{
-			"resource_a": 1,
-			"resource_b": 1,
+			"resource_a": 2,
+			"resource_b": 0,
 		},
 		"res://source/match/units/Helicopter.tscn":
 		{
 			"resource_a": 1,
-			"resource_b": 1,
+			"resource_b": 3,
 		},
 		"res://source/match/units/Drone.tscn":
 		{
-			"resource_a": 1,
-			"resource_b": 1,
+			"resource_a": 2,
+			"resource_b": 0,
 		},
 		"res://source/match/units/Tank.tscn":
 		{
-			"resource_a": 1,
+			"resource_a": 3,
 			"resource_b": 1,
 		},
 	}
 	const PRODUCTION_TIMES = {
-		"res://source/match/units/Worker.tscn": 0.75,
-		"res://source/match/units/Helicopter.tscn": 0.75,
-		"res://source/match/units/Drone.tscn": 0.75,
-		"res://source/match/units/Tank.tscn": 0.75,
+		"res://source/match/units/Worker.tscn": 3.0,
+		"res://source/match/units/Helicopter.tscn": 6.0,
+		"res://source/match/units/Drone.tscn": 3.0,
+		"res://source/match/units/Tank.tscn": 6.0,
 	}
 	const BUILDING_BLUEPRINTS = {
 		"res://source/match/units/CommandCenter.tscn":
@@ -72,49 +72,49 @@ class Units:
 	const CONSTRUCTION_COSTS = {
 		"res://source/match/units/CommandCenter.tscn":
 		{
-			"resource_a": 1,
-			"resource_b": 1,
+			"resource_a": 8,
+			"resource_b": 8,
 		},
 		"res://source/match/units/VehicleFactory.tscn":
 		{
-			"resource_a": 1,
-			"resource_b": 1,
+			"resource_a": 6,
+			"resource_b": 0,
 		},
 		"res://source/match/units/AircraftFactory.tscn":
 		{
-			"resource_a": 1,
-			"resource_b": 1,
+			"resource_a": 4,
+			"resource_b": 4,
 		},
 		"res://source/match/units/AntiGroundTurret.tscn":
 		{
-			"resource_a": 1,
-			"resource_b": 1,
+			"resource_a": 2,
+			"resource_b": 2,
 		},
 		"res://source/match/units/AntiAirTurret.tscn":
 		{
-			"resource_a": 1,
-			"resource_b": 1,
+			"resource_a": 2,
+			"resource_b": 2,
 		},
 	}
 	const DEFAULT_PROPERTIES = {
 		"res://source/match/units/Drone.gd":
 		{
 			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
+			"hp": 3,
+			"hp_max": 3,
 		},
 		"res://source/match/units/Worker.gd":
 		{
-			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
+			"sight_range": 5.0,
+			"hp": 3,
+			"hp_max": 3,
 			"resources_max": 2,
 		},
 		"res://source/match/units/Helicopter.gd":
 		{
-			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
+			"sight_range": 8.0,
+			"hp": 5,
+			"hp_max": 5,
 			"attack_damage": 1,
 			"attack_interval": 1.0,
 			"attack_range": 5.0,
@@ -122,11 +122,11 @@ class Units:
 		},
 		"res://source/match/units/Tank.gd":
 		{
-			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
-			"attack_damage": 1,
-			"attack_interval": 1.0,
+			"sight_range": 8.0,
+			"hp": 5,
+			"hp_max": 5,
+			"attack_damage": 2,
+			"attack_interval": 0.75,
 			"attack_range": 5.0,
 			"attack_domains": [Navigation.Domain.TERRAIN],
 		},
@@ -138,33 +138,33 @@ class Units:
 		},
 		"res://source/match/units/VehicleFactory.gd":
 		{
-			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
+			"sight_range": 8.0,
+			"hp": 8,
+			"hp_max": 8,
 		},
 		"res://source/match/units/AircraftFactory.gd":
 		{
-			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
+			"sight_range": 8.0,
+			"hp": 8,
+			"hp_max": 8,
 		},
 		"res://source/match/units/AntiGroundTurret.gd":
 		{
-			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
-			"attack_damage": 1,
+			"sight_range": 8.0,
+			"hp": 4,
+			"hp_max": 4,
+			"attack_damage": 2,
 			"attack_interval": 1.0,
 			"attack_range": 8.0,
 			"attack_domains": [Navigation.Domain.TERRAIN],
 		},
 		"res://source/match/units/AntiAirTurret.gd":
 		{
-			"sight_range": 10.0,
-			"hp": 10,
-			"hp_max": 10,
-			"attack_damage": 1,
-			"attack_interval": 1.0,
+			"sight_range": 8.0,
+			"hp": 4,
+			"hp_max": 4,
+			"attack_damage": 2,
+			"attack_interval": 0.75,
 			"attack_range": 8.0,
 			"attack_domains": [Navigation.Domain.AIR],
 		},
