65b28a31aecf27e3fc4110ef4b0e01d05ce99855
Pawel Lampe <pawel.lampe@gmail.com>
2023-04-02 16:51:09 +0200

    Simplify randomized turret rotation algorithm

--- source/match/units/traits/RotateRandomlyWhenLookingForTargetsIdle.gd
+++ source/match/units/traits/RotateRandomlyWhenLookingForTargetsIdle.gd
@@ -1,29 +1,10 @@
-# TODO: use simpler randomization model
 extends Node
 
 const WaitingForTargets = preload("res://source/match/units/actions/WaitingForTargets.gd")
 const Structure = preload("res://source/match/units/Structure.gd")
 
-const ROTATION_MULTIPLIER_MARKOVIAN_TRANSITIONS = {
-	0:
-	{
-		0: 0.97,
-		1: 0.015,
-		-1: 0.015,
-	},
-	1:
-	{
-		0: 0.02,
-		1: 0.98,
-		-1: 0.0,
-	},
-	-1:
-	{
-		0: 0.02,
-		1: 0.0,
-		-1: 0.98,
-	},
-}
+const ROTATION_MULTIPLIER_CHANGE_INTERVAL_LB_S = 0.2
+const ROTATION_MULTIPLIER_CHANGE_INTERVAL_UB_S = 0.8
 
 @export var node_to_rotate: NodePath
 @export var rotation_speed = 120.0  # degrees/s
@@ -31,12 +12,21 @@ const ROTATION_MULTIPLIER_MARKOVIAN_TRANSITIONS = {
 var _current_rotation_multiplier = 0
 
 @onready var _unit = get_parent()
+@onready var _timer = find_child("Timer")
+
+
+func _ready():
+	_timer.timeout.connect(_on_rotation_multiplier_change_timer_timeout)
+	_timer.start(
+		randf_range(
+			ROTATION_MULTIPLIER_CHANGE_INTERVAL_LB_S, ROTATION_MULTIPLIER_CHANGE_INTERVAL_UB_S
+		)
+	)
 
 
 func _physics_process(delta):
 	if _unit is Structure and not _unit.is_constructed():
 		return
-	_calculate_new_rotation_multiplier()
 	if (
 		get_node_or_null(node_to_rotate) != null
 		and _unit.action is WaitingForTargets
@@ -47,8 +37,10 @@ func _physics_process(delta):
 		)
 
 
-func _calculate_new_rotation_multiplier():
-	var roulette_wheel = Utils.RouletteWheel.new(
-		ROTATION_MULTIPLIER_MARKOVIAN_TRANSITIONS[_current_rotation_multiplier]
+func _on_rotation_multiplier_change_timer_timeout():
+	_current_rotation_multiplier = randi_range(-1, 1)
+	_timer.start(
+		randf_range(
+			ROTATION_MULTIPLIER_CHANGE_INTERVAL_LB_S, ROTATION_MULTIPLIER_CHANGE_INTERVAL_UB_S
+		)
 	)
-	_current_rotation_multiplier = roulette_wheel.get_value(randf())
