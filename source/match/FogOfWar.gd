55e4d3e96f35a236ba915e6eb8ec0f21ea09d210
Pawel Lampe <pawel.lampe@gmail.com>
2023-04-05 23:20:20 +0200

    Expose fog of war config to the editor

--- source/match/FogOfWar.gd
+++ source/match/FogOfWar.gd
@@ -2,9 +2,11 @@ extends Node3D
 
 const DynamicCircle2D = preload("res://source/generic-scenes-and-nodes/2d/DynamicCircle2D.tscn")
 
-const FOG_CIRCLE_COLOR = Color(0.25, 0.25, 0.25)
-const SHROUD_CIRCLE_COLOR = Color(1.0, 1.0, 1.0)
-const TEXTURE_UNITS_PER_WORLD_UNIT = 2  # px/m
+const DEFAULT_SIZE = Vector2i(100, 100)
+
+@export_range(1, 10) var texture_units_per_world_unit = 2  # px/m
+@export var fog_circle_color = Color(0.25, 0.25, 0.25)
+@export var shroud_circle_color = Color(1.0, 1.0, 1.0)
 
 var _unit_to_circles_mapping = {}
 
@@ -12,9 +14,15 @@ var _unit_to_circles_mapping = {}
 @onready var _fog_viewport = find_child("FogViewport")
 @onready var _fog_viewport_container = find_child("FogViewportContainer")
 @onready var _combined_viewport = find_child("CombinedViewport")
+@onready var _screen_overlay = find_child("ScreenOverlay")
 
 
 func _ready():
+	if _fog_viewport.size == DEFAULT_SIZE:
+		resize(find_parent("Match").find_child("Map").size)
+	_screen_overlay.material_override.set_shader_parameter(
+		"texture_units_per_world_unit", texture_units_per_world_unit
+	)
 	_revealer.hide()
 	find_child("EditorOnlyCircle").queue_free()
 
@@ -39,8 +47,8 @@ func reveal():
 
 
 func resize(map_size: Vector2):
-	_fog_viewport.size = map_size * TEXTURE_UNITS_PER_WORLD_UNIT
-	_combined_viewport.size = map_size * TEXTURE_UNITS_PER_WORLD_UNIT
+	_fog_viewport.size = map_size * texture_units_per_world_unit
+	_combined_viewport.size = map_size * texture_units_per_world_unit
 
 
 func _unit_is_mapped(unit):
@@ -49,19 +57,19 @@ func _unit_is_mapped(unit):
 
 func _map_unit_to_new_circles(unit):
 	var shroud_circle = DynamicCircle2D.instantiate()
-	shroud_circle.color = FOG_CIRCLE_COLOR
-	shroud_circle.radius = unit.sight_range * TEXTURE_UNITS_PER_WORLD_UNIT
+	shroud_circle.color = fog_circle_color
+	shroud_circle.radius = unit.sight_range * texture_units_per_world_unit
 	_fog_viewport.add_child(shroud_circle)
 	var fow_circle = DynamicCircle2D.instantiate()
-	fow_circle.color = SHROUD_CIRCLE_COLOR
-	fow_circle.radius = unit.sight_range * TEXTURE_UNITS_PER_WORLD_UNIT
+	fow_circle.color = shroud_circle_color
+	fow_circle.radius = unit.sight_range * texture_units_per_world_unit
 	_fog_viewport_container.add_sibling(fow_circle)
 	_unit_to_circles_mapping[unit] = [shroud_circle, fow_circle]
 
 
 func _sync_circles_to_unit(unit):
 	var unit_pos_3d = unit.global_transform.origin
-	var unit_pos_2d = Vector2(unit_pos_3d.x, unit_pos_3d.z) * TEXTURE_UNITS_PER_WORLD_UNIT
+	var unit_pos_2d = Vector2(unit_pos_3d.x, unit_pos_3d.z) * texture_units_per_world_unit
 	_unit_to_circles_mapping[unit][0].position = unit_pos_2d
 	_unit_to_circles_mapping[unit][1].position = unit_pos_2d
 
