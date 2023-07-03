2abb5bad87cbd7cd45340822aa5f1df60e44484f
Pawel Lampe <pawel.lampe@gmail.com>
2023-07-03 22:32:30 +0200

    Remove polling from 'RallyPoint'

--- source/match/units/traits/RallyPoint.gd
+++ source/match/units/traits/RallyPoint.gd
@@ -6,8 +6,6 @@ extends Node3D
 
 func _ready():
 	_animation_player.play("idle")
-
-
-func _process(_delta):
-	# TODO: calculate based on signals to avoid polling
 	visible = _unit.is_in_group("selected_units")
+	_unit.selected.connect(show)
+	_unit.deselected.connect(hide)
