320cae6529009ea718ba33bcb18fd521de9ef177
Pawel Lampe <pawel.lampe@gmail.com>
2023-08-27 18:09:26 +0200

    Create dummy structues when they disappear, #79

--- source/match/handlers/UnitVisibilityHandler.gd
+++ source/match/handlers/UnitVisibilityHandler.gd
@@ -1,8 +1,19 @@
 extends Node3D
 
+# TODO: re-use dummies: show/hide them instead of re-creating
+
+const Structure = preload("res://source/match/units/Structure.gd")
+
+var _units_processed_at_least_once = {}
+var _structure_to_dummy_mapping = {}
+
+# TODO: handle dummies properly when changing player visibility
+#       -> dummies are not removed when created and switching player
+
 
 func _ready():
-	MatchSignals.unit_spawned.connect(_on_unit_spawned)
+	MatchSignals.unit_spawned.connect(_recalculate_unit_visibility)
+	MatchSignals.unit_died.connect(_on_unit_died)
 
 
 func _physics_process(_delta):
@@ -25,6 +36,7 @@ func _physics_process(_delta):
 func _recalculate_unit_visibility(unit, revealed_units = null):
 	if unit.is_in_group("revealed_units"):
 		unit.show()
+		_units_processed_at_least_once[unit] = true
 		return
 	var should_be_visible = false
 	if revealed_units == null:
@@ -43,8 +55,34 @@ func _recalculate_unit_visibility(unit, revealed_units = null):
 		):
 			should_be_visible = true
 			break
+	if (
+		unit in _units_processed_at_least_once
+		and unit is Structure
+		and unit.visible != should_be_visible
+	):
+		if unit.visible:
+			_create_dummy_structure(unit)
+		else:
+			_try_removing_dummy_structure(unit)
 	unit.visible = should_be_visible
+	_units_processed_at_least_once[unit] = true
 
 
-func _on_unit_spawned(unit):
-	_recalculate_unit_visibility(unit)
+func _create_dummy_structure(unit):
+	if unit in _structure_to_dummy_mapping:
+		return
+	var dummy = unit.find_child("Geometry").duplicate()
+	dummy.global_transform = unit.find_child("Geometry").global_transform
+	add_child(dummy)
+	_structure_to_dummy_mapping[unit] = dummy
+
+
+func _try_removing_dummy_structure(unit):
+	if unit in _structure_to_dummy_mapping:
+		_structure_to_dummy_mapping[unit].queue_free()
+		_structure_to_dummy_mapping.erase(unit)
+
+
+func _on_unit_died(unit):
+	_units_processed_at_least_once.erase(unit)
+	_structure_to_dummy_mapping.erase(unit)
