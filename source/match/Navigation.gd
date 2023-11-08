8c3b0902beb785bbc2b4e3afb687da2e2350cad7
Pawel Lampe <pawel.lampe@gmail.com>
2023-11-08 23:06:09 +0100

    Fix terrain units moving out of map bounds, #69

--- source/match/Navigation.gd
+++ source/match/Navigation.gd
@@ -6,13 +6,18 @@ var _dummy_agent_rids = {
 	Constants.Match.Navigation.Domain.AIR: null,
 	Constants.Match.Navigation.Domain.TERRAIN: null,
 }
+var _static_obstacles = []
 
 @onready var air = find_child("Air")
 @onready var terrain = find_child("Terrain")
 
+@onready var _match = find_parent("Match")
+
 
 func _ready():
 	_setup_navigation_fixing_timer()
+	await _match.ready
+	_setup_static_obstacles()
 
 
 func get_navigation_map_rid_by_domain(domain):
@@ -25,6 +30,8 @@ func get_navigation_map_rid_by_domain(domain):
 func rebake(map):
 	air.rebake(map)
 	terrain.rebake()
+	_clear_static_obstacles()
+	_setup_static_obstacles()
 
 
 func _setup_navigation_fixing_timer():
@@ -32,6 +39,43 @@ func _setup_navigation_fixing_timer():
 	timer.timeout.connect(_fix_navigation)
 	add_child(timer)
 	timer.start(NAVIGATION_FIXING_TIMER_INTERVAL_S)
+
+
+func _clear_static_obstacles():
+	for static_obstacle in _static_obstacles:
+		NavigationServer3D.free_rid(static_obstacle)
+	_static_obstacles = []
+
+
+func _setup_static_obstacles():
+	if not _static_obstacles.is_empty():
+		return
+	for domain in [
+		Constants.Match.Navigation.Domain.AIR, Constants.Match.Navigation.Domain.TERRAIN
+	]:
+		for edge in [
+			[Vector2(0, 0), Vector2(1, 0)],
+			[Vector2(1, 0), Vector2(1, 1)],
+			[Vector2(1, 1), Vector2(0, 1)],
+			[Vector2(0, 1), Vector2(0, 0)],
+		]:
+			var actual_edge = [edge[0] * _match.map.size, edge[1] * _match.map.size]
+			var obstacle = NavigationServer3D.obstacle_create()
+			NavigationServer3D.obstacle_set_map(obstacle, get_navigation_map_rid_by_domain(domain))
+			(
+				NavigationServer3D
+				. obstacle_set_vertices(
+					obstacle,
+					[
+						Vector3(actual_edge[0].x, -99, actual_edge[0].y),
+						Vector3(actual_edge[0].x, 99, actual_edge[0].y),
+						Vector3(actual_edge[1].x, 99, actual_edge[1].y),
+						Vector3(actual_edge[1].x, -99, actual_edge[1].y),
+					]
+				)
+			)
+			NavigationServer3D.obstacle_set_avoidance_enabled(obstacle, true)
+			_static_obstacles.append(obstacle)
 
 
 func _fix_navigation():
