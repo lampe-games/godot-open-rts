ab49385f7c9f3589287d96a608e2c8b0fecf48fc
Pawel Lampe <pawel.lampe@gmail.com>
2023-11-13 23:08:05 +0100

    Fix air units moving out of map bounds, fixes #69

--- source/match/Navigation.gd
+++ source/match/Navigation.gd
@@ -53,29 +53,22 @@ func _setup_static_obstacles():
 	for domain in [
 		Constants.Match.Navigation.Domain.AIR, Constants.Match.Navigation.Domain.TERRAIN
 	]:
-		for edge in [
-			[Vector2(0, 0), Vector2(1, 0)],
-			[Vector2(1, 0), Vector2(1, 1)],
-			[Vector2(1, 1), Vector2(0, 1)],
-			[Vector2(0, 1), Vector2(0, 0)],
-		]:
-			var actual_edge = [edge[0] * _match.map.size, edge[1] * _match.map.size]
-			var obstacle = NavigationServer3D.obstacle_create()
-			NavigationServer3D.obstacle_set_map(obstacle, get_navigation_map_rid_by_domain(domain))
-			(
-				NavigationServer3D
-				. obstacle_set_vertices(
-					obstacle,
-					[
-						Vector3(actual_edge[0].x, -99, actual_edge[0].y),
-						Vector3(actual_edge[0].x, 99, actual_edge[0].y),
-						Vector3(actual_edge[1].x, 99, actual_edge[1].y),
-						Vector3(actual_edge[1].x, -99, actual_edge[1].y),
-					]
-				)
-			)
-			NavigationServer3D.obstacle_set_avoidance_enabled(obstacle, true)
-			_static_obstacles.append(obstacle)
+		var obstacle = NavigationServer3D.obstacle_create()
+		NavigationServer3D.obstacle_set_map(obstacle, get_navigation_map_rid_by_domain(domain))
+		var obstacle_y = {
+			Constants.Match.Navigation.Domain.AIR: Constants.Match.Air.Y,
+			Constants.Match.Navigation.Domain.TERRAIN: 0,
+		}[domain]
+		NavigationServer3D.obstacle_set_position(obstacle, Vector3(0, obstacle_y, 0))
+		var obstacle_vertices = [
+			Vector3(0, 0, 0),
+			Vector3(0, 0, _match.map.size.y),
+			Vector3(_match.map.size.x, 0, _match.map.size.y),
+			Vector3(_match.map.size.x, 0, 0),
+		]
+		NavigationServer3D.obstacle_set_vertices(obstacle, obstacle_vertices)
+		NavigationServer3D.obstacle_set_avoidance_enabled(obstacle, true)
+		_static_obstacles.append(obstacle)
 
 
 func _fix_navigation():
