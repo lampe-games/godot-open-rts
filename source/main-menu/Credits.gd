f8211c28d263fb264d5f20cc9cf034d8d4bb9ba2
Pawel Lampe <pawel.lampe@gmail.com>
2023-12-27 21:30:05 +0100

    Improve main menu's look'n'feel, #65

--- source/main-menu/Credits.gd
+++ source/main-menu/Credits.gd
@@ -1,5 +1,16 @@
 extends Control
 
+@onready var _rich_text_label = find_child("RichTextLabel")
+
+
+func _ready():
+	_rich_text_label.text = (
+		_rich_text_label
+		. text
+		. replace("CORE_CONTRIBUTORS", tr("CORE_CONTRIBUTORS"))
+		. replace("ASSETS", tr("ASSETS"))
+	)
+
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://source/main-menu/Main.tscn")
