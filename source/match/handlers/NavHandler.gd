0516b1932093be6c8d32326afe63e23e6bc70163
Malf <malte@alfi.li>
2024-08-10 22:16:08 +0200

    add spawn marker func

--- source/match/handlers/NavHandler.gd
+++ source/match/handlers/NavHandler.gd
@@ -1,17 +1,20 @@
 extends Node3D
+
+
+
 const MapCollisionUtils = preload("res://source/match/utils/MapCollisionUtils.gd")
+const _debugmarker = preload("res://source/DebugMarker3D.tscn")
+
 @onready var _map = find_parent("Match").find_child("Map")
 @onready var _gamematch = find_parent("Match")
-var _hmnavmesh = null
-const _debugmarker = preload("res://source/DebugMarker3D.tscn")
 @onready var HeightMapNavMeshClass = load("res://source/match/utils/HeightMapNavMesh.gd")
+
+var _hmnavmesh = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var extents = MapCollisionUtils.compute_terrain_extent(_map)
-	var dm1 = _debugmarker.instantiate()
-	_gamematch.add_child(dm1)
-	#_gamematch.global_position = Vector3(0, 1, 0)
 	var terrain = _map.find_child("Terrain3D")
 	print(terrain.storage.get_height(Vector3(135,0,0)))
 	print(extents)
@@ -23,6 +26,11 @@ func _ready():
 		0.6
 	)
 	pass # Replace with function body.
+
+func _spawn_marker(pos):
+	var dm = _debugmarker.instantiate()
+	_gamematch.add_child(dm)
+	dm.global_position = pos
 
 func find_path(src, dst, costFunc):
 	_hmnavmesh.find_path(costFunc, src, dst)
