2914578dc15286b5069729d5e9449b0814604e55
Pawel Lampe <pawel.lampe@gmail.com>
2024-08-27 22:37:45 +0200

    Make structures affect navigation via obstacles instead of custom colliders

--- source/match/units/traits/MovementObstacle.gd
+++ source/match/units/traits/MovementObstacle.gd
@@ -11,6 +11,13 @@ func _ready():
 	await get_tree().process_frame  # wait for navigation to be operational
 	set_navigation_map(_match.navigation.get_navigation_map_rid_by_domain(domain))
 	_align_unit_position_to_navigation()
+	_affect_navigation_if_needed()
+
+
+func _exit_tree():
+	if affect_navigation_mesh:
+		remove_from_group(Constants.Match.Navigation.DOMAIN_TO_GROUP_MAPPING[domain])
+		MatchSignals.schedule_navigation_rebake.emit(domain)
 
 
 func _align_unit_position_to_navigation():
@@ -20,3 +27,9 @@ func _align_unit_position_to_navigation():
 		)
 		- Vector3(0, path_height_offset, 0)
 	)
+
+
+func _affect_navigation_if_needed():
+	if affect_navigation_mesh:
+		add_to_group(Constants.Match.Navigation.DOMAIN_TO_GROUP_MAPPING[domain])
+		MatchSignals.schedule_navigation_rebake.emit(domain)
