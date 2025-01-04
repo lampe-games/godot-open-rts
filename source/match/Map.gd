f6573cdc0bd7ad5f054528943274c8e9f55f44ea
Pawel Lampe <pawel.lampe@gmail.com>
2025-01-04 20:29:30 +0100

    Make terrain edges soft, closes #73

--- source/match/Map.gd
+++ source/match/Map.gd
@@ -1,10 +1,14 @@
 @tool
 extends Node3D
 
+# TODO: add editor-only 2nd pass shader to 'Terrain' mesh highlighting map boundries
+
+const EXTRA_MARGIN = 2
+
 @export var size = Vector2(50, 50):
 	set(a_size):
 		size = a_size
-		find_child("Terrain").mesh.size = size
+		find_child("Terrain").mesh.size = size + Vector2(EXTRA_MARGIN, EXTRA_MARGIN) * 2
 		find_child("Terrain").mesh.center_offset = Vector3(size.x, 0.0, size.y) / 2.0
 
 
